0770531de640962ead092c0a2081bc3a74cd3679
m0r15 <serenkovav@gmail.com>
2016-02-11 14:56:13 +0300

    init system

--- init.el
+++ init.el
@@ -3,3 +3,17 @@
 
 (defvar user-lib-dir (expand-file-name "lib" user-emacs-directory))
 (defvar lisp-dir (expand-file-name "lisp" user-emacs-directory))
+
+(add-to-list 'load-path lisp-dir)
+(add-to-list 'load-path user-lib-dir)
+
+(setq custom-file (expand-file-name "custom.el" lisp-dir))
+(load custom-file)
+
+;; Add files in lib dir to 'load-path
+(dolist (project (directory-files user-lib-dir t "\\w+"))
+  (when (file-directory-p project)
+        (add-to-list 'load-path project)))
+
+;; Init
+(require 'init-package)
