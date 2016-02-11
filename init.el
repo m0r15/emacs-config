cff6ee8851222304007b9ac48785cc943f3392ce
m0r15 <serenkovav@gmail.com>
2016-02-11 08:25:39 +0300

    Add some start file

---
+++ init.el
@@ -0,0 +1,5 @@
+;;; init.el - emacs config
+;;; Initialisation file for Emacs
+
+(defvar user-lib-dir (expand-file-name "lib" user-emacs-directory))
+(defvar lisp-dir (expand-file-name "lisp" user-emacs-directory))
