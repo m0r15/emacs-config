c1f5bcfb269aebdf6662fbf47555bff1e8995ac3
m0r15 <serenkovav@gmail.com>
2017-03-21 12:38:56 +0300

    Сменил нумерацию строк

--- lisp/init-linum.el
+++ lisp/init-linum.el
@@ -1,8 +1,14 @@
 ;; Linum+ config
-(require-package 'linum+)
+(require-package 'linum)
+;(require 'linum+)
 
 ;(global-nlinum-mode t)
-(setq linum-format "%d ")
-(global-linum-mode l)
+;(setq linum-format "%d ")
+;(global-linum-mode l)
+
+(line-number-mode t) ;; показвать номер строки в mode-line
+(global-linum-mode t) ;; показать номер строк во всех буферах
+(column-number-mode t) ;; показать номер столбца в mode-line
+(setq linum-format " %d") ;; задаем формат нумерации строк
 
 (provide 'init-linum)
